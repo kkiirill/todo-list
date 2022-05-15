772662ecfe7b5320147c2632c8b16bb636847c9a
Kirill Polozenko <kir.polozenko@icloud.com>
2022-05-15 18:12:51 +0300

    todo list done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import  './App.css'
+import Header  from './components/Header/Header'
+import TaskList from './components/TaskList/TaskList'
 
-function App() {
+const App = () => {
+  const [tasks, setTasks] = useState([])
+
+  const addTask = (taskInput) => {
+    if (taskInput) {
+      const item = {
+        id: Math.random().toString(36).substring(2, 9),
+        task: taskInput,
+        complete: false,
+      }
+
+      setTasks([...tasks, item])
+    }
+  }
+
+  const removeTask = (id) => {
+    setTasks([...tasks].filter((task) => {
+      return (
+        task.id !== id
+      )
+    }))
+  }
+
+  const handleToggle = (id) => {
+    setTasks([...tasks].map((task) => 
+      task.id === id ? {...task, complete: !task.complete} : {...task}
+    )
+  )}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header tasks={tasks} addTask={addTask} />
+
+      {tasks.map((task) => {
+        return (
+          <TaskList
+            key={task.id}
+            task={task}
+            removeTask={removeTask}
+            handleToggle={handleToggle}
+          />
+        )
+      })}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
